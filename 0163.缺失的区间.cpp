7ea38be04ca0bc874c6c17b8cd11806414d99bfa
egl1annnn <egl1annn@gmail.com>
2023-10-15 23:06:25 +0800

    10.13

--- 0163.缺失的区间.cpp
+++ 0163.缺失的区间.cpp
@@ -6,5 +6,30 @@
 class Solution {
 public:
     vector<vector<int>> findMissingRanges(vector<int> &nums, int lower, int upper) {
+        vector<vector<int>> missingRanges;
+        // adding a lower bound and upper bound to simplify logic in the loop
+        // dummy node
+        nums.insert(nums.begin(), lower - 1);
+        nums.push_back(upper + 1);
 
+        for (int i = 1; i < nums.size(); i++) {
+            // if adjacent numbers have a difference greater than 1,there is a missing range
+            if (nums[i] - nums[i - 1] > 1) {
+                vector<int> range;
+                // if the range is just a single number, store only that number
+                if (nums[i] - nums[i - 1] == 2) {
+                    range.push_back(nums[i - 1] + 1);
+                    range.push_back(nums[i - 1] + 1);
+
+                } else {// otherwise ,store the start and end of the range
+                    range.push_back(nums[i - 1] + 1);
+                    range.push_back((nums[i] - 1));
+                }
+                missingRanges.push_back((range));
+
+            }
+
+        }
+        return missingRanges;
+    }
 };
