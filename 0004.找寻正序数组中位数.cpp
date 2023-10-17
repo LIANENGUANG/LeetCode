8e22288708d117d8e34e921a6e788fbbba583b32
egl1annnn <egl1annn@gmail.com>
2023-10-17 23:49:46 +0800

    10.15

--- 0004.找寻正序数组中位数.cpp
+++ 0004.找寻正序数组中位数.cpp
@@ -5,43 +5,40 @@ using namespace std;
 
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int> merged(m + n);
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        // 标记三个向量的index值，分别代表两个输入向量的索引和待合并的向量的索引
-        int i = 0, j = 0, k = 0;
-        // 进行归并排序，保证索引不能超过输入向量范围
-        while (i < n && j < m) {
-            if (nums1[i] < nums2[j]) {
-                merged[k++] = nums1[i++];
+        int x = nums1.size();
+        int y = nums2.size();
+        int low = 0, high = x;
 
+        while(low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minX = (partitionX == x) ? INT_MAX : nums1[partitionX];
+
+            int maxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minY = (partitionY == y) ? INT_MAX : nums2[partitionY];
+
+            if(maxX <= minY && maxY <= minX) {
+                if((x + y) % 2 == 0) {
+                    return ((double)max(maxX, maxY) + min(minX, minY)) / 2;
+                } else {
+                    return (double)max(maxX, maxY);
+                }
+            } else if(maxX > minY) {
+                high = partitionX - 1;
             } else {
-                merged[k++] = nums2[j++];
-
+                low = partitionX + 1;
             }
         }
-        // 处理剩余串
-        while (i < n) {
-            merged[k++] = nums1[i++];
-
-        }
-        while (j < m) {
-            merged[k++] = nums2[j++];
-
-        }
-        // 查找中位数，通过两个向量的长度来判断总共元素的奇偶数
-        if ((m + n) % 2 == 0) {
-
-            return ((merged[(m + n) / 2] + merged[(m + n) / 2 - 1]) / 2.0);
-        } else {
-            return (merged[(m + n) / 2]);
-        }
+        throw invalid_argument("Input arrays are not sorted.");
     }
-
 };
-
 int main04() {
     vector num1 = {1, 2};
     vector num2 = {3, 4};
