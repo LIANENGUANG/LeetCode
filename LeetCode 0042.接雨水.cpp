ac51ad7200348b25e1158f1ab37acbfac6f59e01
egl1annnn <egl1annn@gmail.com>
2023-11-06 23:59:34 +0800

    11.06

--- LeetCode 0042.接雨水.cpp
+++ LeetCode 0042.接雨水.cpp
@@ -2,31 +2,28 @@
 // Created by egl1an on 2023/10/7.
 //
 #include "predefs.h"
+
 class Solution {
 public:
-    int trap(vector<int>& heights) {
-        int ans = 0;
-        for (int height : heights) {
-            int accumulatewidth = 0;
-            while (!s.empty() && s.top().height <= height) {
-                int bottom = s.top().height;
-                accumulatewidth += s.top().width;
-                s.pop();
-                if (s.empty()) continue;  // 水从左边流走了
-                // 以bottom 为底的横块水，最高可以到up，左右两侧高度的最小值
-                int up = min(height, s.top().height);
-                ans += accumulatewidth * (up - bottom);
-            }
-            accumulatewidth += 1;  // update width
-            s.push({accumulatewidth, height});
-        }
-        return ans;  // Return the accumulated water
-    }
+    int trap(vector<int>& height) {
+        stack<int> monoStack; // 单调递减栈，存储索引
+        int totalWater = 0;
 
-private:
-    struct Rect {
-        int width;
-        int height;
-    };
-    stack<Rect> s;
+        for (int i = 0; i < height.size(); ++i) {
+            while (!monoStack.empty() && height[i] > height[monoStack.top()]) {
+                int top = monoStack.top();
+                monoStack.pop();
+                if (monoStack.empty()) {
+                    // 如果栈为空，则没有左边界
+                    break;
+                }
+                int distance = i - monoStack.top() - 1; // 计算宽度
+                int bounded_height = min(height[i], height[monoStack.top()]) - height[top]; // 计算高度
+                totalWater += distance * bounded_height; // 计算积水量
+            }
+            monoStack.push(i); // 当前柱子高度小于等于栈顶，或者栈为空，当前索引入栈
+        }
+
+        return totalWater;
+    }
 };
